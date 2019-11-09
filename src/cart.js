d2487aefe8355ac802fbce54c80332dd733881a5
zeglarz <zeglarz@gmail.com>
2019-11-09 17:23:26 +0100

    final not

---
+++ src/cart.js
@@ -0,0 +1,49 @@
+const cartButtonTemplate = `
+
+<style>
+    .cart-button {
+        position: fixed;
+        bottom: 40px;
+        right: 40px;
+        border-radius: 100%;
+        width: 80px;
+        heigth: 80px;
+    }
+</style>
+
+<button type="button" class="cart-button btn btn-secondary btn-lg" data-toggle="modal" data-target="#exampleModalLong"><img src="src/images/cart.svg"/></button>
+
+`;
+
+$('body').append(cartButtonTemplate);
+
+const renderCartModal = () => {
+const cartModalTemplate = `
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ${(JSON.stringify(Object.values(CartItems), null, 4))}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+const modal = document.getElementById('exampleModalLong');
+if (modal) {
+    modal.innerHTML = cartModalTemplate;
+} else {
+    $('body').append(cartModalTemplate);
+}
+};
--- src/main/products.js
+++ src/main/products.js
@@ -11,10 +11,52 @@ function productCard(product) {
     <div class="card-body">
       <h5 class="card-title">${product.name}</h5>
       <p class="card-text">${product.description}</p>
-      <a href="#" class="btn btn-primary">${product.price} zł</a>
+      <button class="btn btn-primary" onclick="Cart.addItem('${product.id}', '${product.name}', ${product.price})">
+      dodaj do koszyka
+      </button>
+    <button class="btn btn-secondary">Zobacz więcej</button>
     </div>
   </div>
     `
 };
 
 document.querySelector("main").innerHTML = productsListContainer;
+
+/*
+var carusel = {`
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    ${` <div class="carousel-item">
+    <img src="..." class="d-block w-100" alt="...">
+  </div>
+  <div class="carousel-item">
+    <img src="..." class="d-block w-100" alt="...">
+  </div>
+</div>`
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+`}
+*/
+
+CartItems = {};
+
+Cart = {
+    items: [],
+    addItem: (productId, productName, productPrice) => {
+        console.log(productId, productName, productPrice);
+        renderCartModal();
+        CartItems[productId] = {productPrice, productName};
+
+        console.log(CartItems);
+}
+}
